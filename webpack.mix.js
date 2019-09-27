eeb51dffb8e8108f911e5b1884cc4c00f0f6d9a1
edo9k <edox86@gmail.com>
2019-09-27 14:53:38 -0300

    First commit
    Mostly automatically added scaffolding from Laravel and Vue.

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+/** @format */
+
+const mix = require('laravel-mix')
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+/* do i need this shit?
+mix.options({
+  hmrOptions: {
+    host: 'localhost',
+    port: '8666'
+  }
+})
+*/
+
+mix.js('resources/js/app.js', 'public/js').sass('resources/sass/app.scss', 'public/css')
